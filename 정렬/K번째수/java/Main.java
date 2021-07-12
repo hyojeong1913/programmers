61d8ce1c35951412d886c1db5557a6985aa5170a
LEE HYO JEONG <hyojeong1913@naver.com>
2021-07-12 23:28:51 +0900

    프로그래머스 자바 'K번째수'

--- 정렬/K번째수/java/Main.java
+++ 정렬/K번째수/java/Main.java
@@ -1,8 +1,9 @@
 import java.util.Arrays;
 
 class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         int[] array = {1, 5, 2, 6, 3, 7, 4};
+
         int[][] commands = {
             {2, 5, 3},
             {4, 4, 1},
@@ -18,8 +19,12 @@ class Main {
         int[] answer = new int[commands.length];
         
         for (int i=0; i<commands.length; i++) {
+
+            // Arrays.copyOfRange(array, i, j) 메소드
+            // : array 의 i 번째부터 (j - 1) 번째까지 총 j - i 개의 원소를 가지는 배열 반환
             int[] tmp = Arrays.copyOfRange(array, commands[i][0] - 1, commands[i][1]);
             
+            // 배열 정렬
             Arrays.sort(tmp);
             
             answer[i] = tmp[commands[i][2] - 1];
