46b5fa4e9af016bf0367a54e952353a447c2fa3f
LEE HYO JEONG <hyojeong1913@naver.com>
2021-11-07 02:29:10 +0900

    프로그래머스 자바 '약수의 합'

---
+++ 연습문제/약수의_합/java/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+
+    public static void main(String[] args) {
+    
+        int[] n = {12 ,5};
+
+        for (int i=0; i<n.length; i++) {
+            
+            System.out.println(solution(n[i]));
+        }
+    }
+    
+    public static int solution(int n) {
+        
+        int answer = 0;
+        
+        for (int i=1; i<=n; i++) {
+            
+            if (n % i == 0) {
+                answer += i;
+            }
+        }
+        
+        return answer;
+    }
+}
